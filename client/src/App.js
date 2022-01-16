61a91f81c4ca943ec0a8eb858e215f1d86f13c56
Michael-Loren <michael.loren@cix.csi.cuny.edu>
2022-01-16 16:54:02 -0500

    add navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,24 @@
 import "./App.css";
 import React from "react";
-import {BrowserRouter, Routes, Route, Link, NavLink} from "react-router-dom"
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Customer from "./components/Customer";
 import Employee from "./components/Employee";
 function App() {
   return (
     <div className="App">
-      <header>
-        
-        
-      </header>
-      <main>
-        <BrowserRouter>
-        
-        
+      <BrowserRouter>
+        <header>
+          <nav>
+            <Link to="/">Home</Link> <Link to="/employee">Employee</Link>
+          </nav>
+        </header>
+        <main>
           <Routes>
-            <Route path="/" element={<Customer/>}/>
-            <Route path="/employee" element={<Employee/>}/>
+            <Route path="/" element={<Customer />} />
+            <Route path="/employee" element={<Employee />} />
           </Routes>
-        </BrowserRouter>
-      </main>
+        </main>
+      </BrowserRouter>
     </div>
   );
 }
