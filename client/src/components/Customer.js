f3a667a4b1e37af65a60d23780990829e0cc5041
Michael-Loren <michael.loren@cix.csi.cuny.edu>
2022-01-16 18:02:02 -0500

    fix onsubmit

--- client/src/components/Customer.js
+++ client/src/components/Customer.js
@@ -3,12 +3,11 @@ import React from "react";
 export default function Customer() {
 
 function onSubmit(e){
-    
     e.preventDefault();
 }
   return (
     <div>
-      <form>
+      <form onSubmit={onSubmit}>
         <select name="food" defaultValue="none">
           <option value="none" disabled hidden>
             Select something
@@ -18,7 +17,7 @@ function onSubmit(e){
           <option value="Pasta">Pasta</option>
           <option value="Raw mushrooms">Raw mushrooms</option>
         </select>
-        <button type="submit" onSubmit={(e) => onSubmit}>Place order</button>
+        <button type="submit" >Place order</button>
       </form>
     </div>
   );
