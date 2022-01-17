45c8f7cdab10e90eba1cfeef2251a8c26aa29220
Michael-Loren <michael.loren@cix.csi.cuny.edu>
2022-01-17 14:12:00 -0500

    Add posting

--- client/src/components/Customer.js
+++ client/src/components/Customer.js
@@ -4,18 +4,31 @@ export default function Customer() {
   const [currentOption, setCurrentOption] = useState("");
 
   function onChange(e) {
-    setCurrentOption(e.target.value)
+    setCurrentOption(e.target.value);
   }
 
-  function onSubmit(e) {
-    if (currentOption){
-    console.log(currentOption)
-    alert("order has been placed!");
-    }
-    else{
-      alert("Select an order");
-    }
+  async function onSubmit(e) {
     e.preventDefault(); //remove later
+    try {
+      console.log(currentOption)
+      const order = {description: currentOption}
+      if (order) {
+        console.log(Object.keys(order));
+        const response = await fetch("http://localhost:5000/todos/", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(order),
+        });
+        console.log(response);
+        
+
+        alert("order has been placed!");
+      } else {
+        alert("Select an order");
+      }
+    } catch (error) {
+      console.error(error.message)
+    }
   }
   return (
     <div>
@@ -27,7 +40,9 @@ export default function Customer() {
           <option value="Cooka da meatball">Cooka da meatball</option>
           <option value="Burger">Burger</option>
           <option value="Pasta">Pasta</option>
-          <option value="Garden mushrooms">Garden mushrooms (you are liable)</option>
+          <option value="Garden mushrooms">
+            Garden mushrooms (you are liable)
+          </option>
           <option value="Food">Food</option>
           <option value="Cardboard">Cardboard + salt</option>
         </select>
