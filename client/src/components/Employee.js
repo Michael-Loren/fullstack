c4511e9a583eca26f3e0ccdb0ced1f4e3f3eaeaf
Michael-Loren <michael.loren@cix.csi.cuny.edu>
2022-01-17 15:20:34 -0500

    Retrieve data from server

--- client/src/components/Employee.js
+++ client/src/components/Employee.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import { useState, useEffect } from 'react';
 
 export default function Employee() {
+    const [currentData, setCurrentData] = useState();
+
+    async function fetchAPI(){
+        const response = await fetch("http://localhost:5000/todos/");
+        const data = await response.json();
+        console.log(data)
+        setCurrentData(data);
+    }
+
+    useEffect(async () => {
+        fetchAPI();
+    }, [])
+    
     return (
         <div>
-            <p>Yoooo</p>
+            
         </div>
     )
 }
