d8b8a0c6c31ee7f57a5bf1d4325c47902d8babc2
Michael-Loren <michael.loren@cix.csi.cuny.edu>
2022-01-17 12:27:34 -0500

    Set up select events

--- client/src/components/Customer.js
+++ client/src/components/Customer.js
@@ -1,23 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Customer() {
+  const [currentOption, setCurrentOption] = useState("");
 
-function onSubmit(e){
-    e.preventDefault();
-}
+  function onChange(e) {
+    setCurrentOption(e.target.value)
+  }
+
+  function onSubmit(e) {
+    if (currentOption){
+    console.log(currentOption)
+    alert("order has been placed!");
+    }
+    else{
+      alert("Select an order");
+    }
+    e.preventDefault(); //remove later
+  }
   return (
     <div>
-      <form onSubmit={onSubmit}>
-        <select name="food" defaultValue="none">
+      <form onSubmit={(e) => onSubmit(e)}>
+        <select name="food" defaultValue="none" onChange={(e) => onChange(e)}>
           <option value="none" disabled hidden>
             Select something
           </option>
           <option value="Cooka da meatball">Cooka da meatball</option>
           <option value="Burger">Burger</option>
           <option value="Pasta">Pasta</option>
-          <option value="Raw mushrooms">Raw mushrooms</option>
+          <option value="Garden mushrooms">Garden mushrooms (you are liable)</option>
+          <option value="Food">Food</option>
+          <option value="Cardboard">Cardboard + salt</option>
         </select>
-        <button type="submit" >Place order</button>
+        <button type="submit">Place order</button>
       </form>
     </div>
   );
