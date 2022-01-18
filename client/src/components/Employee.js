e3f909c3aa168f1e99112f49888574875e925658
Michael-Loren <michael.loren@cix.csi.cuny.edu>
2022-01-17 19:52:53 -0500

    add employee side

--- client/src/components/Employee.js
+++ client/src/components/Employee.js
@@ -1,23 +1,62 @@
-import React from 'react'
-import { useState, useEffect } from 'react';
+import React from "react";
+import { useState, useEffect } from "react";
 
 export default function Employee() {
-    const [currentData, setCurrentData] = useState();
+  const [currentData, setCurrentData] = useState([]);
 
-    async function fetchAPI(){
-        const response = await fetch("http://localhost:5000/todos/");
-        const data = await response.json();
-        console.log(data)
-        setCurrentData(data);
+  async function fetchAPI() {
+    const response = await fetch("http://localhost:5000/todos/");
+    const data = await response.json();
+    console.log(data);
+    setCurrentData(data);
+  }
+
+  useEffect(async () => {
+    fetchAPI();
+  }, []);
+
+  async function onClick(id) {
+    try {
+      const response = await fetch(`http://localhost:5000/todos/${id}`, {
+        method: "DELETE",
+      });
+      console.log(response);
+    } catch (err) {
+      console.error(err.message);
     }
+  }
 
-    useEffect(async () => {
-        fetchAPI();
-    }, [])
-    
-    return (
-        <div>
-            
-        </div>
-    )
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Order</th>
+          </tr>
+        </thead>
+        <tbody>
+          {currentData.map((data) => {
+            return (
+              <tr key={data.todo_id}>
+                <td>
+                  {data.description === "Cardboard"
+                    ? "Cardboard + salt"
+                    : data.description}
+                </td>
+                <td>
+                  <button
+                    onClick={() => {
+                      onClick(data.todo_id);
+                    }}
+                  >
+                    X 
+                  </button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
 }
